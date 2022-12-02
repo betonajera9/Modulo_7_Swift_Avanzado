51b37698248f5b2587b5c49f7d8c4c1af5fc6922
betonajera9 <ing.betonajera@gmail.com>
2022-12-01 23:44:50 -0600

    :tada: begin: Initial project
    Initial project

---
+++ app/src/main/java/dgtic/unam/modulosiete/RecipeAdapter2.kt
@@ -0,0 +1,34 @@
+package dgtic.unam.video
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+import android.widget.ImageView
+import android.widget.TextView
+import dgtic.unam.modulosiete.R
+import dgtic.unam.sonido.ModeloAudio
+
+class RecipeAdapter2(context: Context,private val list:ArrayList<ModeloAudio>): BaseAdapter() {
+    private lateinit var inflter:LayoutInflater
+    init {
+        inflter=LayoutInflater.from(context)
+    }
+    override fun getCount(): Int {
+        return list.size
+    }
+    override fun getItem(p0: Int): Any? {
+        return null
+    }
+    override fun getItemId(p0: Int): Long {
+        return 1
+    }
+    override fun getView(p0: Int, p1: View?, p2: ViewGroup?): View {
+        var view=inflter.inflate(R.layout.list_item,null)
+        val file:TextView= view.findViewById(R.id.title)
+        val image:ImageView=view.findViewById(R.id.image_sound)
+        file.text=list[p0].namefile
+        image.setImageResource(list[p0].nameImage)
+        return view
+    }
+}
